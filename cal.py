ddcc77358941e052739de7dc235784cf7497fb80
alaale <abdiaziz.isse024@gmail.com>
2020-12-19 05:37:17 -0500

    initial commit

---
+++ cal.py
@@ -0,0 +1,28 @@
+print('KUSOO DHAWOOW ALALE ROBOT    ')
+
+
+print ('fadlan si aan kuugu sheego jawaabta saxda ah fadlan sisaxa u raac talaaba walba oo aan kuu sheego')
+
+divide = input ('fadlan kala saar raga iyo dumarka: taabo Enter hadii aad kala saartay')
+
+libin = input('fadlan libin laab tirada raga: taabo Enter hadii aad libin laabtay')
+
+ugayn = input('fadlan ugee sadex tiradii aad libin laabtay ee Raga: taabo Enter hadii ad ugaysay ')
+
+kudhufo = input ('fadlan shan ku dhufo tirada aad hayso hada ee Raga:taabo Enter hadii ku dhufatay')
+
+final = input('fadlan ugee tirada dumarka hadii ay jiraan totalka tirada aad hayso')
+
+ok = int (input('fadlan gali iskugaynta guud ee tirada si aan kuugu sheego inta dumara iyo inta raga'))
+
+lk = ok - 15
+ca = 'to'
+b= len(ca)
+print(b)
+
+lki = [lk]
+x = len(lki)
+print(lki)
+print(x)
+
+print(str(lk))
---
+++ myp.py
@@ -0,0 +1,59 @@
+from colorama import Fore, Back, Style
+import datetime
+print(Fore.GREEN+ 'Xisaabta Guriga')
+print(u'\u2630' * 50)
+
+x = str(datetime.datetime.now())
+
+
+print(u'\u26C4' + '   ' + 'kusoo dhawoow Zara Robot')
+
+print("                                           ")
+who = input (u'\u261B' + '   ' + 'Bishan yaa guriga xisaabtiisa haya: ')
+
+if who == 'dayib':
+    print(u'\u2639' + '   ' + 'waar waa ninkii dayib ahaa soo dhawoow wallo')
+elif who == 'salmaan':
+    print(u'\u263A' + '   ' + 'miskiinkii Alle Salmaan bhai')
+
+elif who == 'suul':
+    print(u'\u263B' + '   ' + 'waar waa suulkii cadaabta ahaa iska soo dhawoow sxb')
+elif who == 'mukhtaar':
+    print(u'\u265c' + '   ' + 'waar waa ninkii mukhtaar ee chess-ta jeclaa ')
+
+elif who == 'abdiaziz':
+   print(u'\u2764' + '   ' + 'waar waa Mulkiilihii soo dhawoow Mudane')
+
+else:
+    print(u'\u26C4' + '   ' + 'barasho wacan mudane  '  + who  + ' ' + 'magacaygu waa zaara waxaan ahay Robot fahmi kara dadka') 
+
+
+
+   
+   
+        
+        
+   
+
+# print(who + ' ' + 'soo dhawoow wllo')
+
+
+
+kiro = int (input( u'\u270F' + '   ' + 'Fadlan gali kirada guriga: '))
+net =   int (input(u'\u270E' + '   ' + 'Fadlan gali lacagta internetka: '))
+gabadha =  int  (input(u'\u270D' + '   ' + 'Fadlan gali gabadha shaqaalaha lacagteeda: '))
+electeric =  int  (input(u'\u2728' + '  ' + 'Fadlan gali lacagta laydhka: '))
+dust =  int (input(u'\u2711' + '   ' + 'Fadlan gali lacagta qashinka: '))
+gas =  int (input( u'\u2712' + '   ' + 'Fadlan gali lacagta gaska: '))
+food =  int (input( u'\u2713' + '   ' + 'Fadlan gali lacagta cuntada: '))
+
+
+
+person =  int  (input(u'\u2A2C' + '   ' + 'imisa qof ayaad ku dagantihiin guriga: '))
+
+
+total = (kiro + net + gabadha + electeric + dust  + gas)
+last = total 
+final = last / 5 + food
+
+print(str(final) + 'tk' + '  ' + 'waxaana xisaabiyay' + ' ' + who + ' ' +  x)
